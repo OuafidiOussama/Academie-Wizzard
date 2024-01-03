d39cc2962cbc054bf20735c72d7485e31f0f05a2
OUAFIDI <o.ouafidi@shellboxes.com>
2024-01-03 09:44:17 +0100

    Creating the faculty entity and its validation, files generated using nestCLI => faculty folder (i only modified the entity and the createFacumtyDTO to test if the table is created in the db)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/connection.config';
+import { FacultyModule } from './faculty/faculty.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig)],
+  imports: [TypeOrmModule.forRoot(databaseConfig), FacultyModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/faculty/dto/create-faculty.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator"
+
+export class CreateFacultyDto {
+    @IsNotEmpty({message: "Please Provide the faculty's name"})
+    @IsString()
+    label : string
+
+    @IsNotEmpty({message: "Please Providethe faculty's Address"})
+    @IsString()
+    address : string
+}

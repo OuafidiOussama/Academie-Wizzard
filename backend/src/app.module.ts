4c9b852b301d0ba106bb8f4f94ee27700c02a262
OUAFIDI <o.ouafidi@shellboxes.com>
2024-01-03 10:40:44 +0100

    Creating the specialty entity, files generated using nestCLI => specialty folder (Only modified the entity file)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/connection.config';
 import { FacultyModule } from './faculty/faculty.module';
 import { SubjectModule } from './subject/subject.module';
+import { SpecialtyModule } from './specialty/specialty.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), FacultyModule, SubjectModule],
+  imports: [TypeOrmModule.forRoot(databaseConfig), FacultyModule, SubjectModule, SpecialtyModule],
   controllers: [AppController],
   providers: [AppService],
 })

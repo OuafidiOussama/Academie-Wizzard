d151d4568866e93d56851d200a6aec4bd4a27682
OUAFIDI <o.ouafidi@shellboxes.com>
2024-01-03 11:15:18 +0100

    Creating the user entity, files generated using nestCLI => user folder (Only modified the entity file)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { databaseConfig } from './database/connection.config';
 import { FacultyModule } from './faculty/faculty.module';
 import { SubjectModule } from './subject/subject.module';
 import { SpecialtyModule } from './specialty/specialty.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), FacultyModule, SubjectModule, SpecialtyModule],
+  imports: [TypeOrmModule.forRoot(databaseConfig), FacultyModule, SubjectModule, SpecialtyModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    Fname: string
+
+    @Column()
+    Lname: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column()
+    phone: number
+
+    @Column()
+    role: string
+}

50c9556ee23454005c20bf1f4fb265e7886fb959
Laaouina18 <laaouinanouhaila2019@gmail.com>
2024-01-04 10:02:11 +0100

    entité note

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/connection.config';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig)],
+  imports: [TypeOrmModule.forRoot(databaseConfig), NoteModule],
   controllers: [AppController],
   providers: [AppService],
 })

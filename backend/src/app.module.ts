77ef2b42054eb309b4b28f40f3134dda4091a6d0
Laaouina18 <laaouinanouhaila2019@gmail.com>
2024-01-04 09:56:10 +0100

    entité département

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/connection.config';
+import { DepartementModule } from './departement/departement.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig)],
+  imports: [TypeOrmModule.forRoot(databaseConfig), DepartementModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/departement/entities/departement.entity.ts
@@ -0,0 +1,3 @@
+export class Departement {
+	label: string;
+}

e029a405f2e5ebf7005178e0de79e113eaddecdc
OUAFIDI <o.ouafidi@shellboxes.com>
2024-01-05 10:00:44 +0100

    Creating the student entity, files generated using nestCLI => student folder (Only modified the entity file)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,9 +9,10 @@ import { SpecialtyModule } from './specialty/specialty.module';
 import { UserModule } from './user/user.module';
 import { DepartementModule } from './departement/departement.module';
 import { ResultModule } from './result/result.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), FacultyModule, ResultModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), FacultyModule, ResultModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
